67a3cdb46e480a6add5b8b6b01b0ca31cae9773d
Lawton Pham <develoots@gmail.com>
2022-10-24 18:37:05 -0700

    added tests, listhelper

---
+++ utils/list_helper.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+const blog = require('../models/blog')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 1) {
+    return blogs[0].likes
+  }
+
+  let sumOfLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+  return sumOfLikes
+}
+
+const favoriteBlog = (blogs) => {
+  let mostLikes = Math.max(...blogs.map(blog => blog.likes))
+  let favorite = blogs.find(({ likes }) => likes === mostLikes)
+
+  return {
+    'title': favorite.title,
+    'author': favorite.author,
+    'likes': favorite.likes
+  }
+}
+
+const mostBlogs = (blogs) => {
+
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
